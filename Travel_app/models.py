4db331ec61e474aa5d104a42ef1a590eac99f754
Prabakaran <praba.caecfd@gmail.com>
2020-06-18 21:09:35 +0530

    Admin-Database-Model in App

--- Travel_app/models.py
+++ Travel_app/models.py
@@ -2,13 +2,20 @@ from django.db import models
 
 
 # Create your models here.
-class Tour:
-    """
-    Instantiates the Travel places with user data
-    """
-    def __init__(self, id, place, desc, price,image):
-        self.id = id
-        self.place = place
-        self.desc = desc
-        self.price = price
-        self.image=image
+# class Tour:
+#     """
+#     Instantiates the Travel places with user data
+#     """
+#     def __init__(self, id, place, desc, price,image):
+#         self.id = id
+#         self.place = place
+#         self.desc = desc
+#         self.price = price
+#         self.image=image
+
+class Destination(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField()
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
